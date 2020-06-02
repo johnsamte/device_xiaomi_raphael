6ee383f3a7e0bc804bda1b5bb88a8ffce6f72c51
Joey Huab <joey@evolution-x.org>
2020-06-02 09:41:11 +0000

    raphael: Update to Coral's June fingerprint
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- init/init_raphael.cpp
+++ init/init_raphael.cpp
@@ -104,7 +104,7 @@ void vendor_load_properties() {
     property_override("org.evolution.build_support_url", "https://t.me/EvolutionXRaphael");
     property_override("ro.apex.updatable", "true");
     property_override("ro.oem_unlock_supported", "0");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ2A.200501.001.B2/6352890:user/release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ3A.200605.001/6392402:user/release-keys");
 
     load_dalvikvm_properties();
 }
