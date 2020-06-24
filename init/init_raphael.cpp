4453031e2aad0af43987405cbb6bc9438c9020cb
AnierinB <anierinb@evolution-x.org>
2020-06-24 03:49:39 +0000

    raphael: init: Format init_raphael.cpp
    - Clang format is bae
    
    Signed-off-by: AnierinB <anierinb@evolution-x.org>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- init/init_raphael.cpp
+++ init/init_raphael.cpp
@@ -27,84 +27,97 @@ using android::init::property_set;
 
 void property_override(char const prop[], char const value[])
 {
-    prop_info *pi;
+	prop_info *pi;
 
-    pi = (prop_info*) __system_property_find(prop);
-    if (pi)
-        __system_property_update(pi, value, strlen(value));
-    else
-        __system_property_add(prop, strlen(prop), value, strlen(value));
+	pi = (prop_info *)__system_property_find(prop);
+	if (pi)
+		__system_property_update(pi, value, strlen(value));
+	else
+		__system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
-void property_override_dual(char const system_prop[], char const vendor_prop[],
-    char const value[])
+void property_override_dual(char const system_prop[], char const vendor_prop[], char const value[])
 {
-    property_override(system_prop, value);
-    property_override(vendor_prop, value);
+	property_override(system_prop, value);
+	property_override(vendor_prop, value);
 }
 
-void load_raphaelglobal() {
-    property_override("ro.product.model", "Mi 9T Pro");
-    property_override("ro.build.product", "raphael");
-    property_override("ro.product.device", "raphael");
-    property_override("ro.build.description", "raphael-user 10 QKQ1.190825.002 V12.0.0.8.QFKMIXM release-keys");
+void load_raphaelglobal()
+{
+	property_override("ro.product.model", "Mi 9T Pro");
+	property_override("ro.build.product", "raphael");
+	property_override("ro.product.device", "raphael");
+	property_override("ro.build.description", "raphael-user 10 QKQ1.190825.002 V12.0.1.0.QFKMIXM release-keys");
 }
 
-void load_raphaelin() {
-    property_override("ro.product.model", "Redmi K20 Pro");
-    property_override("ro.build.product", "raphaelin");
-    property_override("ro.product.device", "raphaelin");
-    property_override("ro.build.description", "raphaelin-user 10 QKQ1.190825.002 V12.0.0.9.QFKINXM release-keys");
+void load_raphaelin()
+{
+	property_override("ro.product.model", "Redmi K20 Pro");
+	property_override("ro.build.product", "raphaelin");
+	property_override("ro.product.device", "raphaelin");
+	property_override("ro.build.description", "raphaelin-user 10 QKQ1.190825.002 V12.0.0.9.QFKINXM release-keys");
 }
 
-void load_raphael() {
-    property_override("ro.product.model", "Redmi K20 Pro");
-    property_override("ro.build.product", "raphael");
-    property_override("ro.product.device", "raphael");
-    property_override("ro.build.description", "raphael-user 10 QKQ1.190825.002 V11.0.8.0.QFKCNXM release-keys");
+void load_raphael()
+{
+	property_override("ro.product.model", "Redmi K20 Pro");
+	property_override("ro.build.product", "raphael");
+	property_override("ro.product.device", "raphael");
+	property_override("ro.build.description", "raphael-user 10 QKQ1.190825.002 V12.0.2.0.QFKCNXM release-keys");
 }
 
 void load_dalvikvm_properties()
 {
-    struct sysinfo sys;
+	struct sysinfo sys;
 
-    sysinfo(&sys);
-    if (sys.totalram < 7000ull * 1024 * 1024) {
-        // 4/6GB RAM
-        property_override("dalvik.vm.heapstartsize", "16m");
-        property_override("dalvik.vm.heaptargetutilization", "0.5");
-        property_override("dalvik.vm.heapmaxfree", "32m");
-    } else {
-        // 8/12/16GB RAM
-        property_override("dalvik.vm.heapstartsize", "24m");
-        property_override("dalvik.vm.heaptargetutilization", "0.46");
-        property_override("dalvik.vm.heapmaxfree", "48m");
-    }
+	sysinfo(&sys);
+	if (sys.totalram < 7000ull * 1024 * 1024)
+	{
+		// 4/6GB RAM
+		property_override("dalvik.vm.heapstartsize", "16m");
+		property_override("dalvik.vm.heaptargetutilization", "0.5");
+		property_override("dalvik.vm.heapmaxfree", "32m");
+	}
+	else
+	{
+		// 8/12/16GB RAM
+		property_override("dalvik.vm.heapstartsize", "24m");
+		property_override("dalvik.vm.heaptargetutilization", "0.46");
+		property_override("dalvik.vm.heapmaxfree", "48m");
+	}
 
-    property_override("dalvik.vm.heapgrowthlimit", "256m");
-    property_override("dalvik.vm.heapsize", "512m");
-    property_override("dalvik.vm.heapminfree", "8m");
+	property_override("dalvik.vm.heapgrowthlimit", "256m");
+	property_override("dalvik.vm.heapsize", "512m");
+	property_override("dalvik.vm.heapminfree", "8m");
 }
 
-void vendor_load_properties() {
-    std::string region = android::base::GetProperty("ro.boot.hwc", "");
+void vendor_load_properties()
+{
+	std::string region = android::base::GetProperty("ro.boot.hwc", "");
 
-    if (region.find("CN") != std::string::npos) {
-        load_raphael();
-    } else if (region.find("INDIA") != std::string::npos) {
-        load_raphaelin();
-    } else if (region.find("GLOBAL") != std::string::npos) {
-        load_raphaelglobal();
-    } else {
-        LOG(ERROR) << __func__ << ": unexcepted region!";
-    }
+	if (region.find("CN") != std::string::npos)
+	{
+		load_raphael();
+	}
+	else if (region.find("INDIA") != std::string::npos)
+	{
+		load_raphaelin();
+	}
+	else if (region.find("GLOBAL") != std::string::npos)
+	{
+		load_raphaelglobal();
+	}
+	else
+	{
+		LOG(ERROR) << __func__ << ": unexcepted region!";
+	}
 
-    property_override("org.evolution.build_donate_url", "https://paypal.me/joeyhuab");
-    property_override("org.evolution.build_maintainer", "Joey Huab");
-    property_override("org.evolution.build_support_url", "https://t.me/EvolutionXRaphael");
-    property_override("ro.apex.updatable", "true");
-    property_override("ro.oem_unlock_supported", "0");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ3A.200605.001/6392402:user/release-keys");
+	property_override("org.evolution.build_donate_url", "https://paypal.me/joeyhuab");
+	property_override("org.evolution.build_maintainer", "Joey Huab");
+	property_override("org.evolution.build_support_url", "https://t.me/EvolutionXRaphael");
+	property_override("ro.apex.updatable", "true");
+	property_override("ro.oem_unlock_supported", "0");
+	property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ3A.200605.001/6392402:user/release-keys");
 
-    load_dalvikvm_properties();
+	load_dalvikvm_properties();
 }
