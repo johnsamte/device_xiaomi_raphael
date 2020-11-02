8b8e13f1dd7ee110a750b663921e0a7a3e35ea35
Joey Huab <joey@evolution-x.org>
2020-11-03 06:40:42 +0900

    raphael: Update to Coral's Nov 2020 fingerprint

--- init/init_raphael.cpp
+++ init/init_raphael.cpp
@@ -120,7 +120,7 @@ void vendor_load_properties()
 	property_override("ro.apex.updatable", "true");
 	property_override("ro.oem_unlock_supported", "0");
 	property_override_multifp("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.bootimage.build.fingerprint",
-	    "ro.vendor.build.fingerprint", "google/coral/coral:11/RP1A.201005.004/6782484:user/release-keys");
+	    "ro.vendor.build.fingerprint", "google/coral/coral:11/RP1A.201105.002/6869500:user/release-keys");
 
 	load_dalvik_properties();
 }
