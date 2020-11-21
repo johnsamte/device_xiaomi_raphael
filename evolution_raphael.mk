3714aaf2853c2306adb74e3608c7cc64f8f736b7
Joey Huab <joey@evolution-x.org>
2020-11-21 18:06:56 +0900

    raphael: Cleanup no longer used exports & unneeded configs

--- evolution_raphael.mk
+++ evolution_raphael.mk
@@ -9,10 +9,6 @@ $(call inherit-product, device/xiaomi/raphael/device.mk)
 # Inherit some common Evolution X stuff.
 EVO_BUILD_TYPE := OFFICIAL
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_INCLUDE_WIFI_EXT := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
 WITH_GAPPS := true
 $(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
