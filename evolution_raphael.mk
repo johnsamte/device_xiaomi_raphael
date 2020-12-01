4f552e4627d94a41ea045da2c6b9ed5a703a17ea
Stallix <joey@evolution-x.org>
2020-12-01 13:09:05 +0000

    raphael: Build FOD animation resources

--- evolution_raphael.mk
+++ evolution_raphael.mk
@@ -8,6 +8,7 @@ $(call inherit-product, device/xiaomi/raphael/device.mk)
 
 # Inherit some common Evolution X stuff.
 EVO_BUILD_TYPE := OFFICIAL
+EXTRA_FOD_ANIMATIONS := true
 TARGET_BOOT_ANIMATION_RES := 1080
 WITH_GAPPS := true
 $(call inherit-product, vendor/evolution/config/common_full_phone.mk)
