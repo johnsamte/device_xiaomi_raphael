84af9649ecbd0e119c448315296e709e94d37bcb
Michael Bestas <mkbestas@gmail.com>
2021-01-06 19:42:24 +0200

    raphael: Disable WiFi display for now
    * Don't show the broken setting until we have a working
      QCOM wfd stack
    
    Change-Id: Ie0637914b853609e27e8c5cb5699364b1717f712

--- system_prop.mk
+++ system_prop.mk
@@ -60,7 +60,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.latch_unsignaled=1 \
     debug.sf.enable_hwc_vds=1 \
-    debug.cpurend.vsync=false
+    debug.cpurend.vsync=false \
+    persist.debug.wfd.enable=0
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
