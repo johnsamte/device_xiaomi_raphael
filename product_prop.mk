93f9d77a3aa5e60aacee3c0ab573550117b73c54
Joey Huab <joey@evolution-x.org>
2021-02-20 11:55:07 +0900

    raphael: Tree cleanup and updates from MIUI 21.1.26
    * Add missing vendor permissions
    * Address missing manifest entries
    * Disable Adaptive connectivity toggle in Settings app
    * Disable Smart Pixels overlay
    * Overlay vendor/etc configs
    * Unify vendor_overlay extraction
    * Update and cleanup duplicate framework configs
    * Update compatibility_matrix
    * Update init.mi_thermald.rc
    * Update init.qcom.usb.sh
    * Update product & system props
    
    Revert "raphael: tune ime padding"
    
    This reverts commit 7e3d18b261937dfbeb03de1d9dc518b407d5e7eb.
    
    Revert "raphael: overlay: enable SmartPixels"
    
    This reverts commit f185bb946fe27930afa4ed40cc7f336688eb4a75.
    
    Signed-off-by: Sherif Rahim <sherifrahim2001@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -1,21 +1,22 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020-2021 The LineageOS Project
+# Copyright (C) 2020-2021 The Evolution X Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Adb
-ifeq ($(TARGET_BUILD_VARIANT),eng)
-# /vendor/default.prop is force-setting ro.adb.secure=1
-# Get rid of that by overriding it in /product on eng builds
+# Blurs
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.secure=0 \
-    ro.adb.secure=0
-endif
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1
+
+# Device
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.build.characteristics=nosdcard
 
 # Dex2oat
 PRODUCT_PRODUCT_PROPERTIES += \
-     dalvik.vm.dex2oat64.enabled=true
+    dalvik.vm.dex2oat64.enabled=true
 
 # Override heap growth limit due to high display density on device
 PRODUCT_PROPERTY_OVERRIDES += \
